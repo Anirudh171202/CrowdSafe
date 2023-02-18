28b3a789f23610f8f814ac01fda7f50f44daf6b9
Archna-1 <archnas2@illinois.edu>
2023-02-18 09:26:09 -0800

    Update sidebar with density tolerance slider

--- client/src/components/SideBar/SideBar.js
+++ client/src/components/SideBar/SideBar.js
@@ -1,15 +1,19 @@
 import {
-  CDBSidebar,
-  CDBSidebarContent,
-  CDBSidebarFooter,
-  CDBSidebarHeader,
-  CDBSidebarMenu,
-  CDBSidebarMenuItem,
+    CDBContainer,
+    CDBSidebar,
+    CDBSidebarContent,
+    CDBSidebarHeader,
+    CDBSidebarMenu,
+    CDBSlider
 } from 'cdbreact';
+
+import { useState } from 'react'
 
 import './SideBar.css'
 
 const SideBar = () => {
+    const [densityTolerance, setDensityTolerance] = useState(80);
+
     return (
         // TODO: Add increasing timer
         <CDBSidebar style={{width: '20%'}}>
@@ -19,19 +23,24 @@ const SideBar = () => {
                 </h4>
             </CDBSidebarHeader>
             <CDBSidebarContent className="sidebar-content" >
-                <CDBSidebarMenu>
-                    <CDBSidebarMenuItem style={{ marginBottom: '23px' }}>
+                <CDBSidebarMenu style={{ padding: '24px' }}>
+                    <CDBContainer style={{ marginBottom: '23px' }}>
                         <div style={{ fontWeight: 'bold', fontSize: '18px'}}>Time Elapsed Since Alert:</div>
                         <h5 style={{ fontSize: '18px'}}>30s</h5>
-                    </CDBSidebarMenuItem>
-                    <CDBSidebarMenuItem style={{ marginBottom: '23px' }}>
+                    </CDBContainer>
+                    <CDBContainer style={{ marginBottom: '23px' }}>
                         <div style={{ fontWeight: 'bold', fontSize: '18px'}}>Number of People in Frame:</div>
                         <h5 style={{ fontSize: '18px'}}>45 people</h5>
-                    </CDBSidebarMenuItem>
-                    <CDBSidebarMenuItem style={{ marginBottom: '23px' }}>
+                    </CDBContainer>
+                    <CDBContainer style={{ marginBottom: '50px' }}>
                         <div style={{ fontWeight: 'bold', fontSize: '18px'}}>Coordinates:</div>
                         <h5 style={{ fontSize: '18px'}}>37.4277938, -122.1765526</h5>
-                    </CDBSidebarMenuItem>
+                    </CDBContainer>
+                    <CDBContainer>
+                        <div style={{ fontWeight: 'bold', fontSize: '18px'}}>Density Tolerance (%):</div>
+                        <h5 style={{ fontSize: '18px'}}>A higher tolerance for density means that you will only see areas of dangerously high compaction, such as in a crowd.</h5>
+                        <CDBSlider value={densityTolerance} tooltip="auto" step={10} setValue={setDensityTolerance} style={{ width: '100%' }} />
+                    </CDBContainer>
                 </CDBSidebarMenu>
             </CDBSidebarContent>
         </CDBSidebar>
@@ -39,3 +48,5 @@ const SideBar = () => {
 }
 
 export default SideBar
+
+// Followed documentation tutorial: https://www.devwares.com/blog/create-responsive-sidebar-in-react/
