bdd0b3c776d912713511e60eb94c8a0575eb69ab
Archna-1 <archnas2@illinois.edu>
2023-02-18 17:15:58 -0800

    Play video automatically

--- client/src/components/Grid/Grid.js
+++ client/src/components/Grid/Grid.js
@@ -1,16 +1,16 @@
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+
+import test from './test2.mp4'
 
 const Grid = () => {
     return (
-        <Col style={{margin: '0px', padding: '0px'}} lg={8}>
-            <Col><video controls style={{ objectFit: 'cover', width: '100%'}}>
-                <source src="test.mp4" type="video/mp4" />
-            </video></Col>
-            <Col><video controls style={{ objectFit: 'cover', width: '100%' }}>
-                <source src="test.mp4" type="video/mp4" />
-            </video></Col>
+        <Col>
+            <video autoPlay muted style={{ objectFit: 'cover', width: '100%'}}>
+                <source src={test} type="video/mp4" />
+            </video>
+            <video autoPlay muted style={{ objectFit: 'cover', width: '100%' }}>
+                <source src={test} type="video/mp4" />
+            </video>
         </Col>
     )
 }
