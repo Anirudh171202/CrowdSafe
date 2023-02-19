49d9f2cef5407e5d643ab821f3445caf4e32e988
Saatvik Billa <svik724@berkeley.edu>
2023-02-18 17:28:42 -0800

    changes to grid

--- client/src/components/Grid/Grid.js
+++ client/src/components/Grid/Grid.js
@@ -4,7 +4,7 @@ import Col from 'react-bootstrap/Col';
 
 const Grid = () => {
     return (
-        <Col style={{margin: '0px', padding: '0px'}} lg={8}>
+        <Col style={{margin: '0px', paddingR: '20px'}} lg={8}>
             <Col><video controls style={{ objectFit: 'cover', width: '100%'}}>
                 <source src="test.mp4" type="video/mp4" />
             </video></Col>
